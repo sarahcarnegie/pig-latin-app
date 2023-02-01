ed6f00a77532962e125827c12dcadcce80dc44f3
sarahcarnegie <carnegie.sarah.k@gmail.com>
2023-02-01 19:43:03 +1100

    add handling of consonants before first vowel

--- pig-latin-app.py
+++ pig-latin-app.py
@@ -1,45 +1,34 @@
 vowels = "aeiou"
 suffix = "ay"
-first_vowel_index = -1 # check this for good practice in python
+
 
 def convert(word):
+    first_vowel_index = -1
     for index, char in enumerate(word):
         if char in vowels:
-            global first_vowel_index
             first_vowel_index = index
             break
     
-if first_vowel_index == -1:
-    print("no vowel in: " + word)
-   
-if first_vowel_index == 0:
-    print(word + suffix)
+    if first_vowel_index == -1:
+        print("no vowel in: " + word)
+    
+    if first_vowel_index == 0:
+        print(word + suffix)
 
-convert("apple")
-convert("eat")
-convert("octopus")
-convert("igloo")
-convert("unicorn") 
-convert("bbbl")
-convert("sleep")
-convert("skip")
-convert("string")
-convert("monkey")
-
-# loop thru letters in the word and check each letter to see if it's a vowel
-# assign to the variable
+    if first_vowel_index > 0: 
+        cons_before_vowel = word[:first_vowel_index:]
+        new_word = word[first_vowel_index::]
+        print(new_word + cons_before_vowel + suffix)
 
 
+test_cases = ("apple", "bbbl", "monkey","sleep","string", "Bandicoot", "pla!ypus", "do1phin")
+for test_wrd in test_cases:
+    convert(test_wrd)
 
-# step 2 Take all letters before the first vowel and place them at word end
-# consider variable = (first vowel index) is always the first vowel 
-# first case = if first vowel index = 0, add ay to the end of the word end
-# else if first vowel index is -1 print error, end
-# else extract the letters from 0 to first vowel index - 1 at word end and add ay
+
 # save file
 # git status - checks which files have changed
 # git add <filename> - track the relevant files
 # git commit -m "this commit will...."
 # git push - pushes changes to github
 
-# I debugged my code using print
