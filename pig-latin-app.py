2bcabc5d0edcd2bb05784cb4e22082a85f616c25
sarahcarnegie <carnegie.sarah.k@gmail.com>
2023-02-02 19:38:27 +1100

    refactor and tidy code, check for invalid words

--- pig-latin-app.py
+++ pig-latin-app.py
@@ -1,30 +1,38 @@
+import re
 vowels = "aeiou"
 suffix = "ay"
 
-
-def convert(word):
-    first_vowel_index = -1
+# first_vowel_index takes a word and returns the index of the first vowel
+# if no vowel is found it returns -1
+def first_vowel_index(word:str) -> int:
     for index, char in enumerate(word):
         if char in vowels:
-            first_vowel_index = index
-            break
-    
-    if first_vowel_index == -1:
-        print("no vowel in: " + word)
-    
-    if first_vowel_index == 0:
-        print(word + suffix)
+            return index
+    return -1
 
-    if first_vowel_index > 0: 
-        cons_before_vowel = word[:first_vowel_index:]
-        new_word = word[first_vowel_index::]
-        print(new_word + cons_before_vowel + suffix)
+# covert take a word and the index of the first vowel(>=0) and return the word in pig-latin
+def convert(word:str, first_vowel_index:int) -> str: 
+    lc_word = word.lower()
+    if first_vowel_index < 0:
+        print("error: index must be 0 or greater") 
+    elif first_vowel_index > 0: 
+        cons_before_vowel = lc_word[:first_vowel_index:]
+        new_word = lc_word[first_vowel_index::]
+        return new_word + cons_before_vowel + suffix
 
+    return word + suffix
 
 test_cases = ("apple", "bbbl", "monkey","sleep","string", "Bandicoot", "pla!ypus", "do1phin")
 for test_wrd in test_cases:
-    convert(test_wrd)
-
+    if bool(re.search(r'\d',test_wrd))==True:        
+        print("invalid word: " + test_wrd)
+    else:
+        index = first_vowel_index(test_wrd)
+        if index < 0:
+            print("no vowel in: " + test_wrd)
+        else:
+            piglatin = convert(test_wrd, index)
+            print(piglatin)
 
 # save file
 # git status - checks which files have changed
